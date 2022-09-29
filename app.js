912309cd913f80edf527a6480aa2f25263c24bb8
Axel Teisseire <mr.teisseire@gmail.com>
2022-09-29 15:57:24 +0200

    Add JavaScript Elements

--- app.js
+++ app.js
@@ -2,15 +2,25 @@
 const rollDice = document.querySelector('#rollDiceButton')
 const holdScore = document.querySelector('#holdButton')
 const newGame = document.querySelector('#newgameButton')
+const currentButton = document.querySelector('#currentButton')
+const currentButton2 = document.querySelector('#currentButton2')
 const player1 = document.querySelector('#player1')
 const player2 = document.querySelector('#player2')
 let globalPoints = document.querySelector('#globalPts')
 let globalPoints2 = document.querySelector('#globalPts2')
+let currentPoints = document.querySelector('#currentScore')
+let currentPoints2 = document.querySelector('#currentScore2')
 
 const rollTheDice = () => 
 {
     let diceResult = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
     return diceResult;
+}
+
+if(diceResult === 1) {
+    
+} else {
+
 }
 
 rollDice.addEventListener('click', rollTheDice);
