b4cadd27a8476fac2a6b7edaa369b41897ad19ae
Axel Teisseire <mr.teisseire@gmail.com>
2022-09-29 11:42:09 +0200

    add JavaScript elements and uninstall Jquery pack

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-$(($) => {
-    alert("salut")
+alert("salut")
+let rollDice = document.querySelector('#rollDiceButton');
+rollDice.addEventListener('click', () => {
+    let diceResult = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+    console.log(diceResult);
 });
