58cc36dc43e4ec2254d6db4edd069ddd7397a722
Axel Teisseire <mr.teisseire@gmail.com>
2022-09-29 18:22:00 +0200

    create function to roll the dice and change the player round

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
 
-const rollDice = document.querySelector('#rollDiceButton')
-const holdScore = document.querySelector('#holdButton')
-const newGame = document.querySelector('#newgameButton')
-const currentButton = document.querySelector('#currentButton')
-const currentButton2 = document.querySelector('#currentButton2')
-const player1 = document.querySelector('#player1')
-const player2 = document.querySelector('#player2')
-let globalPoints = document.querySelector('#globalPts')
-let globalPoints2 = document.querySelector('#globalPts2')
-let currentPoints = document.querySelector('#currentScore')
-let currentPoints2 = document.querySelector('#currentScore2')
-let diceResult;
+let actualScore;
+let activePlayer;
+let canvasElement = document.querySelector('#canvasElement')
 
-startGame();
-
-function startGame() 
+const startGame= () => 
 {
+    actualScore = 0;
+    activePlayer = 0;
     globalPoints.textContent = '0';
     globalPoints2.textContent = '0';
     currentPoints.textContent = '0';
     currentPoints2.textContent = '0';
 }
 
-class DiceGame 
+startGame();
+
+const rollTheDice = () => 
 {
-    constructor(player, dice, round) {
-        this.player = player;
-        this.dice = dice;
-        this.round = round;
+    let diceResult = Math.floor(Math.random() * 6) + 1;
+
+    let diceStyleAnimation;
+
+    if (diceResult !== 1) 
+    {
+        actualScore += diceResult;
+        let current;
+    } else {
+        changeRound()
     }
+}
+
+const changeRound = () => 
+{
+
 }
