657bcb0c4b266c929ccb4981e8cec218e1ad8996
Axel Teisseire <mr.teisseire@gmail.com>
2022-09-30 16:55:23 +0200

    Create scene, camera and renderer for animationDice module

--- animateDice.js
+++ animateDice.js
@@ -1,3 +1,14 @@
 import * as THREE from 'https://unpkg.com/three@0.144.0/build/three.module.js';
 
 let canvasElement = document.querySelector('#canvasElement');
+
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(75, canvasElement.innerWidth / canvasElement.innerHeight, 0.1, 1000);
+camera.position.x = 3;
+
+let renderer = new THREE.WebGLRenderer(
+    {
+        canvas: canvasElement
+    }
+);
+
