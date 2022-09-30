f74dda0bedea7abaf3ac2c2dcfc103e3d602fe39
Axel Teisseire <mr.teisseire@gmail.com>
2022-09-30 11:17:19 +0200

    Add javascript elements

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 
 let actualScore;
 let activePlayer;
+let globalPoints = document.querySelector('#globalPts')
+let globalPoints2 = document.querySelector('#globalPts2')
+let currentPoints1 = document.querySelector('#currentScore')
+let currentPoints2 = document.querySelector('#currentScore2')
 let canvasElement = document.querySelector('#canvasElement')
+const rollButton = document.querySelector('#rollDiceButton')
+const holdButton = document.querySelector('#holdButton')
 
-const startGame= () => 
+const startGame = () => 
 {
     actualScore = 0;
-    activePlayer = 0;
+    activePlayer = 1;
     globalPoints.textContent = '0';
     globalPoints2.textContent = '0';
-    currentPoints.textContent = '0';
+    currentPoints1.textContent = '0';
     currentPoints2.textContent = '0';
 }
 
@@ -24,13 +30,25 @@ const rollTheDice = () =>
     if (diceResult !== 1) 
     {
         actualScore += diceResult;
-        let current;
+        let current = document.querySelector(`currentScore${activePlayer}`)
+        current.textContent = actualScore;
     } else {
-        changeRound()
+        changeTheRound()
     }
 }
 
-const changeRound = () => 
+const changeTheRound = () => 
 {
+    if (activePlayer === 1) {
+        activePlayer = 2
+    } else {
+        activePlayer = 1
+        actualScore = 0
+    }
 
+    currentPoints1.textContent = '0';
+    currentPoints2.textContent = '0';
 }
+
+rollButton.addEventListener('click', rollTheDice);
+holdButton.addEventListener('click', changeTheRound);
