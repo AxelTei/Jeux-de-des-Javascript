88ee8a7a706669258ef5f4a5a307d91c72792b39
Axel Teisseire <mr.teisseire@gmail.com>
2022-09-30 17:57:04 +0200

    Create box for dice without animation yet

--- animateDice.js
+++ animateDice.js
@@ -1,14 +1,31 @@
-import * as THREE from 'https://unpkg.com/three@0.144.0/build/three.module.js';
+import * as THREE from 'https://unpkg.com/three@0.122.0/build/three.module.js';
 
-let canvasElement = document.querySelector('#canvasElement');
-
+// Création de la scène, de la caméra et du renderer
+let canvasElement = document.getElementById('canvasElement');
 let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(75, canvasElement.innerWidth / canvasElement.innerHeight, 0.1, 1000);
-camera.position.x = 3;
-
+let camera = new THREE.PerspectiveCamera(70, canvasElement.width / canvasElement.height, 0.1, 1000);
+camera.position.z = 4;
 let renderer = new THREE.WebGLRenderer(
     {
         canvas: canvasElement
     }
 );
 
+// Création du mât
+let poleGeometry = new THREE.BoxGeometry(3, 3, 3);
+let poleMaterial = new THREE.MeshStandardMaterial({
+    color: 'red',
+    roughness: 0.3
+});
+let pole = new THREE.Mesh(poleGeometry, poleMaterial);
+scene.add(pole);
+
+
+// Lumière
+let light = new THREE.PointLight(0xFFFFFF, 1, 50);
+light.position.set(3, 2, 3);
+scene.add(light);
+
+// Rendu de la scène
+renderer.setClearColor('white'); // Bleu ciel au bord de la plage
+renderer.render(scene, camera);
